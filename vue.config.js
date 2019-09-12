a5f39e394646e4898a958b26c1d8bed1a1e28ca5
yingjieweb <1184061078@qq.com>
2019-09-12 19:30:07 +0800

    'color'

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-    configureWebpack:{
-        resolve:{/*给路径配置别名*/
-            'assets':'@/assets',
-            'common':'@/common',
-            'components':'@/components',
-            'network':'@/network',
-        }
+  configureWebpack:{
+    resolve:{/*给路径配置别名*/
+      alias:{
+        'assets':'@/assets',
+          'common':'@/common',
+          'components':'@/components',
+        'network':'@/network',
+      }
     }
+  }
 }
