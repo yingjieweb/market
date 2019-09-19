60ce8c47caba02a7a84508a61d919729c4ff4187
yingjieweb <1184061078@qq.com>
2019-09-19 20:20:13 +0800

    'RecommendView'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,48 @@
 <template>
-    <h2>首页</h2>
+  <div id="app">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners = "banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Home"
+  import NavBar from 'components/common/navbar/NavBar'
+  import HomeSwiper from './childComps/HomeSwiper'
+  import RecommendView from './childComps/RecommendView'
+
+  import {getHomeMultidata} from "network/home";
+  /*import Swiper from 'components/common/swiper/Swiper'
+  import SwiperItem from 'components/common/swiper/SwiperItem'*/
+  //import {Swiper,SwiperItem} from 'components/common/swiper'
+
+  export default {
+    name: "Home",
+    components:{
+      NavBar,
+      HomeSwiper,
+      RecommendView
+    },
+    data(){
+      return {
+        banners:[],
+        recommends:[]
+      }
+    },
+    created() {
+      //1.请求多个数据
+      getHomeMultidata().then(res => {
+        //console.log(res);
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
     }
+  }
 </script>
 
 <style scoped>
-
+  .home-nav{
+    background-color: #1296DB;
+    color: white;
+  }
 </style>
