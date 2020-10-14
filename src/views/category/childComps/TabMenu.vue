d2d4cfb5b89c3c6d923de1aa035250891875e95f
yingjieweb <1184061078@qq.com>
2020-10-14 09:43:08 +0800

    finish Category comp and function

---
+++ src/views/category/childComps/TabMenu.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="tab-menu">
+    <div class="menu-list-item" :class="{active: index===currentIndex}" v-for="(item, index) in categories" :key="index" @click="itemClick(index)">
+      {{item.title}}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabMenu",
+    props:{
+      categories:Array
+    },
+    data(){
+      return{
+        currentIndex:0
+      }
+    },
+    methods:{
+      itemClick(index){
+        this.currentIndex = index;
+        this.$emit('selectItem',index);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-menu {
+    background-color: #f6f6f6;
+    height: 100%;
+    width: 100px;
+    box-sizing: border-box;
+  }
+
+  .menu-list-item {
+    height: 45px;
+    line-height: 45px;
+    text-align: center;
+    font-size: 14px;
+  }
+
+  .menu-list-item.active {
+    font-weight: 700;
+    color: #1296DB;
+    background-color: #fff;
+    border-left: 3px solid #1296DB;
+  }
+</style>
