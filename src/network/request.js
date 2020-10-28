f66972e6caca3ccfffbd9a7b950425e6bd879bd2
yingjieweb <1184061078@qq.com>
2020-10-28 11:21:21 +0800

    adjust axios request baseURL

--- src/network/request.js
+++ src/network/request.js
@@ -4,8 +4,9 @@ import axios from 'axios'
 export function request(config) {
   //1.创建axios实例
   const instance = axios.create({
-    //baseURL: 'http://123.207.32.32:8000/api/wh',
-    baseURL: 'http://106.54.54.237:8000/api/wh',
+    // baseURL: 'http://123.207.32.32:8000/api/wh',
+    // baseURL: 'http://106.54.54.237:8000/api/wh',
+    // baseURL: 'http://152.136.185.210:8000/api/wh',
     timeout: 10000
   })
 
